cda0d8e473bf5afc329c8fa8baa1efe222890657
Joe Glanville <josephglanville@gmail.com>
2023-04-11 16:32:33 +0100

    Build a Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Text, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize='60px' />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
