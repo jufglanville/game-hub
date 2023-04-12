79d44e5113ca60bfd433f643b32a55d8e4fb5709
Joe Glanville <josephglanville@gmail.com>
2023-04-12 11:07:39 +0100

    Highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import { Genre } from "./hooks/useGenres";
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
-
-
   return (
     <Grid
       templateAreas={{
@@ -17,8 +15,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
       templateColumns={{
-        base: '1fr',
-        lg: '200px 1fr',
+        base: "1fr",
+        lg: "200px 1fr",
       }}
     >
       <GridItem area="nav">
@@ -26,7 +24,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+          <GenreList
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
   const { data: genres, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -23,7 +24,7 @@ const GenreList = ({onSelectGenre}: Props) => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">
               {genre.name}
             </Button>
           </HStack>
